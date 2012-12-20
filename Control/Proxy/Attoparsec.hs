8720c364c7d0e6e8bc36f3f00ce95ce9c35e6bf9
Renzo Carbonara <gnuk0001@gmail.com>
2012-12-19 23:31:19 -0300

    More docs, cleaning up

--- Control/Proxy/Attoparsec.hs
+++ Control/Proxy/Attoparsec.hs
@@ -1,106 +1,162 @@
-{-# LANGUAGE DeriveDataTypeable
-           , KindSignatures #-}
+-- | This module provides utilities to turn your Attoparsec @'Parser' a b@ into
+-- a 'Proxy' that parses 'a' values into 'b' values as they flow downstream.
 
-module Control.Proxy.Attoparsec where
--- module Control.Proxy.Attoparsec (
---   ParserError(..),
---   parserD,
---   dropBadChunks,
---   dropBadInput,
---   throwParsingErrors,
---   ) where
+module Control.Proxy.Attoparsec
+  ( -- * Usage example
+    -- $example-simple
 
-import           Prelude hiding (null, drop, length)
+    -- ** Handling errors.
+    -- $example-errors
+
+    -- * Parsing Proxy
+    parserD
+  , parsingPipe
+  , defaultParsingPipe
+    -- ** Some useful controllers
+  , skipMalformedChunks
+  , skipMalformedInput
+  , throwParsingErrors
+  , limitInputLength
+  , module Control.Proxy.Attoparsec.Types
+  ) where
+
 import           Control.Proxy
-import           Control.Proxy.Trans.Either as PE
-import qualified Data.Text as T
-import qualified Data.ByteString as BS
-import           Data.Attoparsec.Types
-import qualified Data.Attoparsec.Text as AT
-import qualified Data.Attoparsec.ByteString as ABS
-import           Data.Typeable (Typeable)
-
-
--- | A class for valid Attoparsec input types.
-class AttoparsecInput a where
-    -- | Run a 'Parser'.
-    parse :: Parser a b -> a -> IResult a b
-    -- | @null a@ tests whether @a@ is empty.
-    null :: a -> Bool
-    -- | @drop n a@ ignores the first @n@ elements from @a@ and returns the rest.
-    drop :: Int -> a -> a
-    -- | @length a@ returns the number of elements in @a@.
-    length :: a -> Int
-
-instance AttoparsecInput BS.ByteString where
-    parse = ABS.parse
-    null = BS.null
-    drop = BS.drop
-    length = BS.length
-
-instance AttoparsecInput T.Text where
-    parse = AT.parse
-    null = T.null
-    drop = T.drop
-    length = T.length
+import           Control.Proxy.Attoparsec.Types
+import qualified Control.Proxy.Trans.Either as PE
+import qualified Control.Proxy.Trans.Either as PE
+import           Data.Attoparsec.Types      (Parser(..), IResult(..))
+import           Prelude                    hiding (drop, length, null)
 
 
 
--- | Status of the parsing proxy.
-data ParserStatus a
-  = Idle
-  -- ^ There is no 'Parser' running.
-  | Parsing Int
-  -- ^ There is a 'Parser' running. The 'Int' parameter indicates the length of
-  -- the input consumed so far.
-  | Failed a ParserError
-  -- ^ A 'Parser' has failed parsing. The @a@ parameter is the input that had
-  -- not yet been consumed when the failure occurred. The 'ParseError' provides
-  -- details about the failure.
-  deriving (Show)
-
-
--- | Length of the input consumed so far.
-pstLength :: ParserStatus a -> Int
-pstLength (Parsing n) = n
-pstLength _           = 0
-
--- | A parse error as returned by Attoparsec.
-data ParserError = ParserError
-    { errorContexts :: [String]  -- ^ Contexts where the error occurred.
-    , errorMessage  :: String    -- ^ Error message.
-    } deriving (Show, Typeable)
-
-
-
--- | Input chunk supplied to the parsing proxy.
-data ParserSupply a
-  = Start a
-  -- ^ Start a new parser and feed it with @a@.
-  | Continue a
-  -- ^ Continue feeding the running parser with @a@.
-  deriving (Show)
-
-
--- * Parsing Proxy
-
--- | Proxy parsing 'ParserSupply' @a@ values flowing downstream into @b@ values.
+-- $example-simple
 --
--- Sends 'ParserStatus' @a@ values upstream.
-type ParsingProxy (p :: * -> * -> * -> * -> (* -> *) -> * -> *) a b
-  = p (ParserStatus a) (ParserSupply a) () b
+-- We'll write a simple 'Parser' that turns 'Text' like /“Hello John Doe.”/
+-- into some @'Name' \"John Doe\"@, and then make a 'Pipe' that turns those
+-- 'Text' values flowing downstream into 'Name' values flowing downstream.
+--
+-- In this example we are using 'Text', but we may as well use 'ByteString'.
+-- Also, the 'OverloadedStrings' language extension lets us write our parser
+-- easily.
+--
+-- > {-# LANGUAGE "OverloadedStrings" #-}
+-- >
+-- > import Control.Proxy
+-- > import Control.Proxy.Attoparsec
+-- > import Control.Proxy.Trans.Either
+-- > import Data.Attoparsec.Text
+-- > import Data.Text
+-- >
+-- > data Name = Name Text
+-- >           deriving (Show)
+-- >
+-- > hello :: Parser Name
+-- > hello = fmap Name $ "Hello" .*> skipSpace >> takeWhile1 (/='.') <*. "."
+--
+-- We are done with our parser, now lets make a simple 'Pipe' out of it.
+--
+-- > helloPipe1 :: (Proxy p, Monad m) => () -> Pipe (EitherP BadInput p) Text Name m r
+-- > helloPipe1 = defaultParsingPipe hello
+--
+-- As the type indicates, this 'Pipe' recieves 'Text' values from upstream and
+-- sends 'Name' values downstream. Through the 'EitherP' proxy transformer we
+-- report upstream errors due to bad input.
+--
+-- We need some sample input.
+--
+-- > input1 :: [Text]
+-- > input1 =
+-- >   [ "Hello Kate."
+-- >   , "Hello Mary."
+-- >   ]
+--
+-- Now we can try our parsing pipe. We'll use @'fromListS' input1@ as our
+-- input source, which sends downstream one element from the list at a time.
+-- We'll call each of these elements a /chunk/. So, @'fromListS' input1@ sends
+-- two /chunks/ of 'Text' downstream.
+--
+-- >>> runProxy . runEitherK $ fromListS input1 >-> helloPipe1 >-> printD
+-- Name "Kate"
+-- Name "Mary"
+-- Right ()
+--
+-- We have acomplished our goal.
 
 
--- | 'ParsingProxy' parsing 'ParserSupply' @a@ values flowing 'D'ownstream into
--- @b@ values, using the given 'Parser' @a b@.
+-- $example-errors
 --
--- An upstream 'ParsingProxyControl' should supply new input as requested by
--- receiving a 'ParserStatus' @a@ value reporting the current status of this
--- 'ParsingProxy'.
+-- Let's try with some more complex input.
 --
--- The input given from upstream is a 'ParserSupply' @a@ which may provide more
--- input to be fed to a partial parsing result, or discard that partial result
--- and instead have the input fed to a new parsing process.
+-- > input2 :: [Text]
+-- > input2 =
+-- >   [ "Hello Alice."
+-- >   , "Hello Bob."
+-- >   , "Hello"
+-- >   , "Hello"
+-- >   , "Hello World."
+-- >   , "HexHello Jon."
+-- >   , "H"
+-- >   , "ello Ann"
+-- >   , "."
+-- >   , "Hello Jean-Luc."
+-- >   ]
+--
+-- >>> runProxy . runEitherK $ fromListS input2 >-> helloPipe1 >-> printD
+-- Name "Alice"
+-- Name "Bob"
+-- Name "HelloHello World"
+-- Left (MalformedInput {miParserErrror = ParserError {errorContexts = [], errorMessage = "Failed reading: takeWith"}})
+--
+-- The simple @helloPipe1@ 'ParsingProxy' we built, when a parsing error is
+-- arises, aborts its execution by throwing a 'BadInput' value in the 'EitherP'
+-- proxy transformer. That might be enough if you are certain your input is
+-- always well-formed, but sometimes you may prefer to just ignore the
+-- particular input that caused the parser to fail and continue parsing new
+-- input.
+--
+-- Handlers for two common scenarios are provided, you use them to build your
+-- parsing pipe using 'parsingPipe', instead of the previously used
+-- 'defaultParsingPipe'.
+--
+-- ['skipMalformedChunks']
+--   Skips the malformed /chunk/ being parsed and requests a new chunk to be
+--   parsed from start.
+--
+--   > helloPipe2 :: (Proxy p, Monad m) => () -> Pipe p Text Name m r
+--   > helloPipe2 = parsingPipe skipMalformedChunks $ parserD hello
+--
+--   >>> runProxy $ fromListS input2 >-> helloPipe2 >-> printD
+--   Name "Alice"
+--   Name "Bob"
+--   Name "HelloHello World"
+--   Name "Ann"
+--   Name "Jean-Luc"
+--
+-- ['skipMalformedInput']
+--   Skips single pieces of the malformed /chunk/, one at a time, until parsing
+--   succeds. It requests a new /chunk/ if needed.
+--
+--   > helloPipe3 :: (Proxy p, Monad m) => () -> Pipe p Text Name m r
+--   > helloPipe3 = parsingPipe skipMalformedInput $ parserD hello
+--
+--   >>> runProxy $ fromListS input2 >-> helloPipe3 >-> printD
+--   Name "Alice"
+--   Name "Bob"
+--   Name "HelloHello World"
+--   Name "Jon"
+--   Name "Ann"
+--   Name "Jean-Luc"
+--
+--
+-- Notice from those examples how we do not get Left
+
+-- | 'ParsingProxy' using the given @'Parser' a b@ to parse @a@ values flowing
+-- downstream into @b@ values.
+--
+-- When more input is needed, a @'ParsingStatus' a@ value reporting the current
+-- parsing status is sent upstream, and in exchange a @'ParsingSupply' a@ value
+-- is expected, containing more input to be parsed and directives on how to
+-- use it (see 'ParsingSupply').
 parserD :: (Proxy p, Monad m, AttoparsecInput a)
         => Parser a b
         -> ()
@@ -110,7 +166,7 @@ parserD parser () = runIdentityP . forever $ start Idle
     start = req (parse parser)
     req k status = request status >>= \x -> case x of
         Start chunk    -> enough chunk >>= processNew
-        Continue chunk -> enough chunk >>= process (pstLength status) k
+        Resume chunk -> enough chunk >>= process (pstLength status) k
       where enough chunk | null chunk = req k status
                          | otherwise  = return chunk
     processNew = process 0 (parse parser)
@@ -122,65 +178,78 @@ parserD parser () = runIdentityP . forever $ start Idle
           if null rest then start Idle
                        else processNew rest
 
--- * Parsing Proxy Control
---
--- TODO better explain the purpose of these
+    -- | Length of the input consumed so far.
+    pstLength (Parsing n) = n
+    pstLength _           = 0
 
 
--- | Proxy supplying proper @a@ input to a 'ParsingProxy' downstream.
---
--- Recieves 'ParserStatus' @a@ from downstream.
-type ParsingProxyControl (p :: * -> * -> * -> * -> (* -> *) -> * -> *) a
-  = p () a (ParserStatus a) (ParserSupply a)
 
--- | If a downstream parsing proxy reports a parser failure, skip the whole
--- chunk being processed and keep processing new input.
+
+
+-- | If a downstream 'ParsingProxy' reports a parser failure, skip the whole
+-- chunk being processed and start processing new input.
 --
 -- Useful when an entire chunk is supposed to be well-formed, so it can be
 -- safely skipped if it is not.
-skipBadChunks :: (Monad m, Proxy p, AttoparsecInput a)
-              => ParserStatus a
-              -> p () a (ParserStatus a) (ParserSupply a) m r
-skipBadChunks = runIdentityK . foreverK $ go
-  where go (Parsing _)  = request () >>= respond . Continue
-        go _            = request () >>= respond . Start
+skipMalformedChunks :: (Monad m, Proxy p, AttoparsecInput a)
+                    => ParsingStatus a -> ParsingControl p a m r
+skipMalformedChunks = runIdentityK . foreverK $ go
+  where go (Failed _ _)  = request Idle >>= respond . Start . supplyChunk
+        go x             = request x    >>= respond
 
 
--- | If a downstream 'ParsingProxy' reports a parser failure, keep skipping bits
--- of input until the parser succeeds.
-skipBadInput :: (Monad m, Proxy p, AttoparsecInput a)
-             => ParserStatus a -> ParsingProxyControl p a m r
-skipBadInput = runIdentityK . foreverK $ go
-  where go (Failed rest _) = do let rest' = drop 1 rest
-                                if null rest'
-                                   then request () >>= respond . Continue
-                                   else respond $ Continue rest'
-        go (Parsing _)     = request () >>= respond . Continue
-        go _               = request () >>= respond . Start
+-- | If a downstream 'ParsingProxy' reports a parser failure, keep skipping
+-- bits of input until the parsing succeeds, and then resume normal operation.
+skipMalformedInput :: (Monad m, Proxy p, AttoparsecInput a)
+                   => ParsingStatus a -> ParsingControl p a m r
+skipMalformedInput = runIdentityK . foreverK $ go
+  where go (Failed rest _) = do
+          let rest' = drop 1 rest
+          if null rest'
+            then request Idle >>= respond . Resume . supplyChunk
+            else respond $ Resume rest'
+        go x = request x >>= respond
 
--- | If a downstream 'ParsingProxy' reports a parser failure, throw the
--- relevant 'ParserError' @a@.
-throwParsingErrors
-  :: (Monad m, Proxy p, AttoparsecInput a)
-  => ParserStatus a
-  -> ParsingProxyControl (EitherP ParserError p) a m r
+
+
+-- | If a downstream 'ParsingProxy' reports a parser failure, then throw
+-- a 'MalformedInput' error.
+throwParsingErrors :: (Monad m, Proxy p, AttoparsecInput a)
+                   => ParsingStatus a
+                   -> ParsingControl (PE.EitherP BadInput p) a m r
 throwParsingErrors = foreverK $ go
-  where go (Failed _ e) = PE.throw e
-        go (Parsing _)  = request () >>= respond . Continue
-        go _            = request () >>= respond . Start
+  where go (Failed _ e) = PE.throw $ MalformedInput e
+        go x            = request x >>= respond
 
-data InputError = InputTooLong Int
-                 deriving (Show)
 
 
 -- | If a downstream 'ParsingProxy' doesn't produce a value after having
--- consumed input least lenght @n@, then throw a 'InputTooLong' error.
-limitInputLength
-  :: (Monad m, Proxy p, AttoparsecInput a)
-  => Int
-  -> ParserStatus a
-  -> (EitherP InputError p) (ParserStatus a) (ParserSupply a)
-     (ParserStatus a) (ParserSupply a) m r
+-- consumed input least lenght @n@, then throw an 'InputTooLong' error.
+limitInputLength :: (Monad m, Proxy p, AttoparsecInput a)
+                 => Int -> ParsingStatus a
+                 -> ParsingControl (PE.EitherP BadInput p) a m r
 limitInputLength n = foreverK $ go
   where go (Parsing m) | m >= n = PE.throw $ InputTooLong m
         go x                    = request x >>= respond
+
+-- | Pipe parsing @a@ values flowing downstream into @b@ values through the
+-- given 'ParsingProxy', controled by the given 'ParsingControl'.
+--
+-- The default control behaviour is to ignore any 'ParsingStatus' and
+-- always 'Resume' parsing.
+parsingPipe :: (Monad m, Proxy p, AttoparsecInput a)
+            => (ParsingStatus a -> ParsingControl p a m r)
+            -> (() -> ParsingProxy p a b m r)
+            -> () -> Pipe p a b m r
+parsingPipe control parsingp = incoming >-> control >-> parsingp
+  where incoming _ = runIdentityP . forever $ request () >>= respond . Resume
+
+
+-- | Pipe parsing @a@ values flowing downstream into @b@ values using the given
+-- @'Parser' a b@. Throws 'MalformedInput' on parsing errors.
+--
+-- > defaultParsingPipe = parsingPipe throwParsingErrors . parserD
+defaultParsingPipe :: (Monad m, Proxy p, AttoparsecInput a)
+                   => Parser a b -> () -> Pipe (PE.EitherP BadInput p) a b m r
+defaultParsingPipe = parsingPipe throwParsingErrors . parserD
+
---
+++ Control/Proxy/Attoparsec/Types.hs
@@ -0,0 +1,136 @@
+{-# LANGUAGE DeriveDataTypeable
+           , KindSignatures     #-}
+
+module Control.Proxy.Attoparsec.Types
+  ( -- * Attoparsec
+    -- Integration with Attoparsec 'Parser'.
+    AttoparsecInput(..)
+  , ParserError(..)
+    -- * Proxy types
+  , ParsingProxy
+  , ParsingControl
+    -- ** Inter-proxy communication
+  , ParsingStatus(..)
+  , ParsingSupply(..)
+  , supplyChunk
+    -- * Error types
+  , BadInput(..)
+  ) where
+
+import           Control.Proxy
+import qualified Data.Attoparsec.ByteString as ABS
+import qualified Data.Attoparsec.Text       as AT
+import           Data.Attoparsec.Types
+import qualified Data.ByteString            as BS
+import qualified Data.Text                  as T
+import           Data.Typeable
+
+
+-- | A class for valid Attoparsec input types.
+class AttoparsecInput a where
+    -- | Run a 'Parser'.
+    parse :: Parser a b -> a -> IResult a b
+    -- | @null a@ tests whether @a@ is empty.
+    null :: a -> Bool
+    -- | @drop n a@ ignores the first @n@ elements from @a@ and returns the rest.
+    drop :: Int -> a -> a
+    -- | @length a@ returns the number of elements in @a@.
+    length :: a -> Int
+    -- | @empty@ returns the empty value for @a@.
+    empty :: a
+
+instance AttoparsecInput BS.ByteString where
+    parse = ABS.parse
+    null = BS.null
+    drop = BS.drop
+    length = BS.length
+    empty = BS.empty
+
+instance AttoparsecInput T.Text where
+    parse = AT.parse
+    null = T.null
+    drop = T.drop
+    length = T.length
+    empty = T.empty
+
+
+
+-- | Type synonym for a 'Proxy' which:
+--
+--  - Sends upstream: 'ParsingStatus' @a@
+--
+--  - Receives upstream: 'ParsingSupply' @a@
+--
+--  - Sends downstream: 'b'
+--
+--  - Receives downstream: @()@
+type ParsingProxy (p :: * -> * -> * -> * -> (* -> *) -> * -> *) a b
+  = p (ParsingStatus a) (ParsingSupply a) () b
+
+
+-- | Type synonym for a 'Proxy' which:
+--
+--  - Sends upstream / receives downstream: 'ParsingStatus' @a@
+--
+--  - Receives upstream / sends downstream: 'ParsingSupply' @a@
+--
+
+type ParsingControl (p :: * -> * -> * -> * -> (* -> *) -> * -> *) a
+   = p (ParsingStatus a) (ParsingSupply a) (ParsingStatus a) (ParsingSupply a)
+
+
+-- | Status of a 'ParsingProxy'.
+data ParsingStatus a
+  -- | There is no current parsing activity.
+  -- Receiving 'Resume' @a@ from upstream would feed input @a@ to a new
+  -- 'Parser'.
+  = Idle
+  -- | There is a 'Parser' running, waiting for more @a@ input.
+  -- Receiving 'Resume' @a@ from upstream would feed input @a@ to the running
+  -- 'Parser'.
+  | Parsing
+    { psLength :: Int -- ^Length of input consumed so far.
+    }
+  -- | A 'Parser' has failed parsing.
+  -- Receiving 'Resume' @a@ from upstream would feed input @a@ to a new
+  -- 'Parser'.
+  | Failed
+    { psLeftover :: a -- ^Input not yet consumed when the failure occurred.
+    , psError    :: ParserError -- ^Error found while parsing.
+    }
+  deriving (Show)
+
+
+-- | A parser error as returned by Attoparsec.
+data ParserError = ParserError
+    { errorContexts :: [String]  -- ^ Contexts where the error occurred.
+    , errorMessage  :: String    -- ^ Error message.
+    } deriving (Show, Typeable)
+
+
+-- | Input chunk supplied to the 'ParsingProxy'.
+data ParsingSupply a
+  = Start a
+  -- ^ Start a new parsing activity, feed it with @a@.
+  | Resume a
+  -- ^ Feed @a@ to the current parsing activity, as explained by
+  -- 'ParsingStatus'.
+  deriving (Show)
+
+
+-- | Get the input chunk from a 'ParsingSupply'.
+supplyChunk :: ParsingSupply a -> a
+supplyChunk (Start  x) = x
+supplyChunk (Resume x) = x
+
+
+-- | Reasons for a input to be unnaceptable.
+data BadInput
+  = InputTooLong
+    { itlLenght :: Int -- ^ Length of the input.
+    }
+  | MalformedInput
+    { miParserErrror :: ParserError -- ^ Error found while parsing.
+    }
+  deriving (Show)
+
