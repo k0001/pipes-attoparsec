19f61806e3815de8214357eba6f089a3fc801330
Renzo Carbonara <gnuk0001@gmail.com>
2013-01-03 15:09:05 -0300

    parseWith and parsingWith

--- src/Control/Proxy/Attoparsec/Types.hs
+++ src/Control/Proxy/Attoparsec/Types.hs
@@ -14,6 +14,8 @@ module Control.Proxy.Attoparsec.Types
     -- * Attoparsec integration
   , AttoparsecInput(..)
   , ParserError(..)
+  , parseWith
+  , parsingWith,
   ) where
 
 import qualified Data.Attoparsec.ByteString as ABS
@@ -21,7 +23,7 @@ import qualified Data.Attoparsec.Text       as AT
 import           Data.Attoparsec.Types
 import qualified Data.ByteString            as BS
 import qualified Data.Text                  as T
-
+import           Prelude                    hiding (null)
 
 
 -- | Status of a parsing 'Proxy'.
@@ -98,4 +100,36 @@ instance AttoparsecInput T.Text where
     null = T.null
     drop = T.drop
     length = T.length
+
+
+
+--- TODO: find a better home for these functions.
+
+
+-- | Run a parser with an initial input string, and a monadic action
+-- that can supply more input if needed.
+parseWith :: (Monad m, AttoparsecInput a, Show a)
+          => m a
+          -- ^ An action that will be executed to provide the parser
+          -- with more input, if necessary. If the action returns an
+          -- 'empty', then it's assumed no more input is available.
+          -> Parser a b
+          -> Maybe a
+          -- ^ Optional initial input for the parser.
+          -> m (Maybe a, Either ([String], String) b)
+parseWith refill p Nothing  = parseWith refill p . Just =<< refill
+parseWith refill p (Just s) = step $ parse p s
+  where
+    step (Partial k)  = step . k =<< refill
+    step (Done t r)   = return (mayChunk t, Right r)
+    step (Fail t c m) = return (mayChunk t, Left (c, m))
+
+    mayChunk t | null t    = Nothing
+               | otherwise = Just t
+
+-- | 'parseWith' with the order of arguments changed.
+parsingWith :: (Monad m, AttoparsecInput a, Show a)
+            => Parser a b -> Maybe a -> m a
+            -> m (Maybe a, Either ([String], String) b)
+parsingWith p ms refill = parseWith refill p ms
 
