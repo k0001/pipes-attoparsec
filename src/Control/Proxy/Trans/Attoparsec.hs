9700120dcd9dcfcf2a8075c0ee85e06a4c018c23
Renzo Carbonara <gnuk0001@gmail.com>
2013-03-20 20:21:50 -0300

    ParseP: unharcode SomeException

--- src/Control/Proxy/Trans/Attoparsec.hs
+++ src/Control/Proxy/Trans/Attoparsec.hs
@@ -1,6 +1,8 @@
 {-# LANGUAGE GeneralizedNewtypeDeriving #-}
 {-# LANGUAGE TypeFamilies #-}
 
+{-# OPTIONS_GHC -fno-warn-unused-do-bind #-}
+
 module Control.Proxy.Trans.Attoparsec
   ( -- * ParseP proxy transformer
     ParseP(..)
@@ -15,8 +17,6 @@ module Control.Proxy.Trans.Attoparsec
   , parseD
   ) where
 
-
-import qualified Control.Exception              as Ex
 import           Control.Applicative            (Applicative(..), (<$>))
 import           Control.MFunctor               (MFunctor)
 import           Control.Monad                  (MonadPlus)
@@ -34,33 +34,31 @@ import           Data.Attoparsec.Types          (Parser)
 import           Prelude                        hiding (length, null, splitAt)
 
 
-newtype ParseP s p a' a b' b m r
-  = ParseP { unParseP :: S.StateP s (E.EitherP Ex.SomeException p) a' a b' b m r }
+newtype ParseP e s p a' a b' b m r
+  = ParseP { unParseP :: S.StateP s (E.EitherP e p) a' a b' b m r }
   deriving (Functor, Applicative, Monad, MonadTrans, MonadPlus,
             MonadIO, MFunctor, P.Proxy, P.MonadPlusP, P.MonadIOP)
 
-instance ProxyTrans (ParseP s) where
+instance ProxyTrans (ParseP e s) where
   liftP = ParseP . liftP . liftP
 
-instance PFunctor (ParseP s) where
+instance PFunctor (ParseP e s) where
   hoistP nat = wrap . (nat .) . unwrap
     where wrap   = ParseP . S.StateP . (E.EitherP .)
           unwrap = (E.runEitherP .) . S.unStateP . unParseP
 
-runParseK :: s -> (t -> ParseP s p a' a b' b m r)
-          -> (t -> p a' a b' b m (Either Ex.SomeException (r, s)))
-runParseK s k q = runParseP s (k q)
-
-runParseP :: s
-          -> ParseP s p a' a b' b m r
-          -> p a' a b' b m (Either Ex.SomeException (r, s))
-runParseP s = E.runEitherP . S.runStateP s . unParseP
-
-instance (P.Proxy p, Monad m) => MonadState (ParseP s p a' a b' b m) where
-  type StateType (ParseP s p a' a b' b m) = s
+instance (P.Proxy p, Monad m) => MonadState (ParseP e s p a' a b' b m) where
+  type StateType (ParseP e s p a' a b' b m) = s
   get   = ParseP (S.StateP (\s -> E.right (s ,s)))
   put s = ParseP (S.StateP (\_ -> E.right ((),s)))
 
+runParseK :: s -> (t -> ParseP e s p a' a b' b m r)
+          -> (t -> p a' a b' b m (Either e (r, s)))
+runParseK s k q = runParseP s (k q)
+
+runParseP :: s -> ParseP e s p a' a b' b m r
+          -> p a' a b' b m (Either e (r, s))
+runParseP s = E.runEitherP . S.runStateP s . unParseP
 
 -- | Pipe input flowing downstream up to length @n@. Return any leftovers.
 takeInputD'
@@ -76,9 +74,8 @@ takeInputD' = P.runIdentityK . go where
           then go (n - length p) ()
           else return (Just s)
 
-
 -- | 'ParseP specialized for Attoparsec integration.
-type AttoparsecP a = ParseP (Maybe a)
+type AttoparsecP a = ParseP ParserError (Maybe a)
 
 -- | Get any leftovers from the 'AttoparsecP' state.
 getLeftovers
@@ -97,7 +94,7 @@ takeLeftovers
   :: (Monad m, P.Proxy p, AttoparsecInput a)
   => Int -> (AttoparsecP a p) a' a b' b m (Maybe a)
 takeLeftovers n = do
-  lo <- getLeftovers
+  lo <- get
   case fmap (splitAt n) lo of
     Nothing    -> return Nothing
     Just (p,s) -> put (mayInput s) >> return (mayInput p)
@@ -127,5 +124,5 @@ parseD parser = (p >-> P.unitU) () where
   p () = ParseP (S.StateP (\s -> do
            (er,s') <- parseWith (P.request ()) parser s
            case er of
-             Left e  -> E.throw (Ex.toException e)
+             Left e  -> E.throw e
              Right r -> return (r,s') ))
