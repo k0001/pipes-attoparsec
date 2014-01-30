70596080b02162a02f0af7c3f1be951624b19445
Renzo Carbonara <gnuk0001@gmail.com>
2014-01-30 03:39:03 -0300

    Cosmetic

--- src/Pipes/Attoparsec.hs
+++ src/Pipes/Attoparsec.hs
@@ -1,7 +1,7 @@
 -- | @pipes@ utilities for incrementally running @attoparsec@-based parsers
 
 {-# LANGUAGE DeriveDataTypeable #-}
-{-# LANGUAGE RankNTypes #-}
+{-# LANGUAGE RankNTypes         #-}
 
 module Pipes.Attoparsec (
     -- * Parsing
@@ -18,28 +18,31 @@ module Pipes.Attoparsec (
     , ParsingError(..)
     ) where
 
-import Control.Exception (Exception)
-import Data.Data (Data, Typeable)
-import Data.Monoid (Monoid(mempty))
-import Data.ByteString (ByteString)
-import Data.Text (Text)
-import Control.Monad.Trans.Error (Error)
-import Data.Attoparsec.Types (IResult(..))
-import qualified Data.Attoparsec.Types as Attoparsec
+import           Control.Exception          (Exception)
+import           Control.Monad.Trans.Error  (Error)
 import qualified Data.Attoparsec.ByteString
 import qualified Data.Attoparsec.Text
+import           Data.Attoparsec.Types      (IResult (..))
+import qualified Data.Attoparsec.Types      as Attoparsec
+import           Data.ByteString            (ByteString)
 import qualified Data.ByteString
+import           Data.Data                  (Data, Typeable)
+import           Data.Monoid                (Monoid (mempty))
+import           Data.Text                  (Text)
 import qualified Data.Text
-import Pipes ((>->), for)
-import qualified Pipes.Parse as Pipes
-import Pipes.Parse
-import qualified Pipes.Prelude as P
+import           Pipes                      (for, (>->))
+import           Pipes.Parse
+import qualified Pipes.Parse                as Pipes
+import qualified Pipes.Prelude              as P
 
--- | Convert an @attoparsec@ parser to a @pipes-parse@ parser
+--------------------------------------------------------------------------------
+
+-- | Convert an @attoparsec@ 'Attoparsec.Parser' to a @pipes-parse@
+-- 'Pipes.Parser'.
 parse
-    :: (Monad m, ParserInput t)
-    => Attoparsec.Parser t a
-    -> Pipes.Parser t m (Either ParsingError a)
+  :: (Monad m, ParserInput a)
+  => Attoparsec.Parser a b                    -- ^ Attoparsec parser
+  -> Pipes.Parser a m (Either ParsingError b) -- ^ Pipes parser
 parse parser = do
     x <- parseL parser
     return $ case x of
@@ -47,28 +50,27 @@ parse parser = do
         Right (_, a) -> Right a
 {-# INLINABLE parse #-}
 
-{-| Convert a stream of 'ParserInput' to a producer of parsed values. This
-    producer returns 'Nothing' when end-of-input is reached successfully,
-    otherwise it returns a 'ParsingError' and the leftovers. The leftovers
-    include any input that it was in the process of being parsed.
--}
+
+-- | Convert a stream of 'ParserInput' to a producer of parsed values.
+--
+-- This producer returns 'Nothing' when end-of-input is reached successfully,
+-- otherwise it returns a 'ParsingError' and the leftovers. The leftovers
+-- include any input that it was in the process of being parsed.
 parsed
-    :: (Monad m, ParserInput t)
-    => Attoparsec.Parser t a
-    -> Producer t m r
-    -> Producer a m (Maybe (ParsingError, Producer t m r))
-    -- ^
+  :: (Monad m, ParserInput a)
+  => Attoparsec.Parser a b  -- ^ Attoparsec parser
+  -> Producer a m r
+  -> Producer b m (Maybe (ParsingError, Producer a m r))
 parsed parser p = for (parsedL parser p) (yield . snd)
 {-# INLINABLE parsed #-}
 
 
-{-| Like 'parse', but also returns the length of input consumed to parse the
-    value
--}
+-- | Like 'parse', but also returns the length of input consumed to parse the
+-- value.
 parseL
-    :: (Monad m, ParserInput t)
-    => Attoparsec.Parser t a
-    -> Pipes.Parser t m (Either ParsingError (Int, a))
+    :: (Monad m, ParserInput a)
+    => Attoparsec.Parser a b
+    -> Pipes.Parser a m (Either ParsingError (Int, b))
 parseL parser = StateT $ \p -> do
     x <- next (p >-> P.filter (/= mempty))
     case x of
@@ -86,17 +88,14 @@ parseL parser = StateT $ \p -> do
         Done t r        -> return (Right (len - _length t, r), yield t >> p)
 {-# INLINABLE parseL #-}
 
-{-| Like 'parsed', except this tags each parsed value with the length of input
-    consumed to parse the value
--}
+
+-- | Like 'parsed', except this tags each parsed value with the length of input
+-- consumed to parse the value.
 parsedL
-    :: (Monad m, ParserInput t)
-    => Attoparsec.Parser t a
-    -- ^
-    -> Producer t m r
-    -- ^
-    -> Producer (Int, a) m (Maybe (ParsingError, Producer t m r))
-    -- ^
+    :: (Monad m, ParserInput a)
+    => Attoparsec.Parser a b    -- ^ Attoparsec parser
+    -> Producer a m r
+    -> Producer (Int, b) m (Maybe (ParsingError, Producer a m r))
 parsedL parser = go
   where
     go p = do
@@ -109,10 +108,9 @@ parsedL parser = go
                        Right  a   -> yield a >> go p'
 {-# INLINABLE parsedL #-}
 
-{-| Like 'Pipes.Parse.isEndOfInput', except that it also consumes and discards
-    leading empty chunks
--}
-isEndOfParserInput :: (Monad m, ParserInput t) => Parser t m Bool
+-- | Like 'Pipes.Parse.isEndOfInput', except that it also consumes and discards
+-- leading empty chunks.
+isEndOfParserInput :: (Monad m, ParserInput a) => Parser a m Bool
 isEndOfParserInput = go
   where
     go = do
@@ -127,15 +125,19 @@ isEndOfParserInput = go
                     return False
 {-# INLINABLE isEndOfParserInput #-}
 
+--------------------------------------------------------------------------------
+
 -- | A class for valid @attoparsec@ input types
 class (Eq a, Monoid a) => ParserInput a where
     _parse  :: Attoparsec.Parser a b -> a -> IResult a b
     _length :: a -> Int
 
+-- | Strict 'ByteString'.
 instance ParserInput ByteString where
     _parse  = Data.Attoparsec.ByteString.parse
     _length = Data.ByteString.length
 
+-- | Strict 'Text'.
 instance ParserInput Text where
     _parse  = Data.Attoparsec.Text.parse
     _length = Data.Text.length
